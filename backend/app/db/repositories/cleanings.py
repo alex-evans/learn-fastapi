6838d1e3a4e90b5b54d7bcc0d4372bfc8158f893
Alex Evans <alex.evans.d@gmail.com>
2021-05-17 21:44:26 -0500

    Finished part 4 - Testing FastAPI endpoings with Docker and pytest

--- backend/app/db/repositories/cleanings.py
+++ backend/app/db/repositories/cleanings.py
@@ -1,14 +1,18 @@
 
 from app.db.repositories.base import BaseRepository
-from app.models.cleaning import CleaningCreate
-from app.models.cleaning import CleaningUpdate
-from app.models.cleaning import CleaningInDB
+from app.models.cleaning import CleaningCreate, CleaningUpdate, CleaningInDB
 
 
 CREATE_CLEANING_QUERY = '''
     INSERT INTO cleanings (name, description, price, cleaning_type)
     VALUES (:name, :description, :price, :cleaning_type)
     RETURNING id, name, description, price, cleaning_type;
+'''
+
+GET_CLEANING_BY_ID_QUERY = '''
+    SELECT id, name, description, price, cleaning_type
+    FROM cleanings
+    WHERE id = :id;
 '''
 
 
@@ -20,5 +24,13 @@ class CleaningsRepository(BaseRepository):
     async def create_cleaning(self, *, new_cleaning: CleaningCreate) -> CleaningInDB:
         query_values = new_cleaning.dict()
         cleaning = await self.db.fetch_one(query=CREATE_CLEANING_QUERY, values=query_values)
+
+        return CleaningInDB(**cleaning)
+
+    async def get_cleaning_by_id(self, *, id: int) -> CleaningInDB:
+        cleaning = await self.db.fetch_one(query=GET_CLEANING_BY_ID_QUERY, values={'id': id})
+
+        if not cleaning:
+            return None
 
         return CleaningInDB(**cleaning)
--- backend/app/db/tasks.py
+++ backend/app/db/tasks.py
@@ -1,4 +1,5 @@
 
+import os
 from fastapi import FastAPI
 from databases import Database
 import logging
@@ -10,7 +11,8 @@ logger = logging.getLogger(__name__)
 
 
 async def connect_to_db(app: FastAPI) -> None:
-    database = Database(DATABASE_URL, min_size=2, max_size=10)  # these can be configured in config as well
+    DB_URL = f'{DATABASE_URL}_test' if os.environ.get('TESTING') else DATABASE_URL
+    database = Database(DB_URL, min_size=2, max_size=10)  # these can be configured in config as well
 
     try:
         await database.connect()
